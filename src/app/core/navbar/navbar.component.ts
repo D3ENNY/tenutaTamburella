fc9d408ff820af1e63e00ebc0c27632e4d8488fe
D3ENNY <denysraimondi06@gmail.com>
2024-02-17 16:36:41 +0100

    add faether icons

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import * as feather from 'feather-icons'
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  ngAfterViewInit() {
+    feather.replace();
+  }
+  
 }
