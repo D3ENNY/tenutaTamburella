ea8aed764788124824799f13f9f1e4bbe704f801
D3ENNY <denysraimondi06@gmail.com>
2024-02-17 16:37:58 +0100

    setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/**/**/*.{html,ts}",
+    "./src/**/**/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
