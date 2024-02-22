f86d4ec8a9ddcc9a1b1ed9ff2a09316306351ac3
D3ENNY <denysraimondi06@gmail.com>
2024-02-22 16:58:11 +0100

    try add interval in flowbite carousel & responsive image carousel

--- src/app/model/home-carousel/home-carousel.component.ts
+++ src/app/model/home-carousel/home-carousel.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import feather from 'feather-icons';
-
-import { initFlowbite } from 'flowbite';
+import { Carousel } from 'flowbite';
 
 
 @Component({
@@ -11,10 +10,52 @@ import { initFlowbite } from 'flowbite';
   templateUrl: './home-carousel.component.html',
   styleUrl: './home-carousel.component.scss'
 })
-export class HomeCarouselComponent {
+export class HomeCarouselComponent{
+
+  @ViewChild('carouselRef', {static:true}) carouselEl: ElementRef<HTMLDivElement> | null
+  @ViewChild('itemOneRef', {static:true}) item1El: ElementRef<HTMLDivElement> | null
+  @ViewChild('itemTwoRef', {static:true}) item2El: ElementRef<HTMLDivElement> | null
+  @ViewChild('itemTwoRef', {static:true}) item3El: ElementRef<HTMLDivElement> | null
+  @ViewChild('itemTwoRef', {static:true}) item4El: ElementRef<HTMLDivElement> | null
+  @ViewChild('itemTwoRef', {static:true}) item5El: ElementRef<HTMLDivElement> | null
+  carousel: Carousel | null = null
+
+  constructor() {
+    this.carouselEl = null
+    this.item1El = null
+    this.item2El = null
+    this.item3El = null
+    this.item4El = null
+    this.item5El = null
+  }
 
   ngAfterViewInit(): void {
     feather.replace();
+
+    if (this.carouselEl /*&& this.item1El && this.item2El && this.item3El && this.item4El && this.item5El*/){
+      this.carousel = new Carousel(
+        //element
+        this.carouselEl.nativeElement, 
+        //items
+        [
+          // { position: 0, el: this.item1El.nativeElement },
+          // { position: 1, el: this.item2El.nativeElement },
+          // { position: 2, el: this.item3El.nativeElement },
+          // { position: 3, el: this.item4El.nativeElement },
+          // { position: 4, el: this.item5El.nativeElement },
+        ], 
+        //options
+        {
+          defaultPosition: 0,
+          interval: 10000,
+        },
+        //istance options
+        {
+          id: 'default-carousel',
+          override: true
+        }
+      )
+    }
   }
 
 }
