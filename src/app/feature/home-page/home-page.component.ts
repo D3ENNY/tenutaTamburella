0914430e38a61213c46be7303260f6026cb315a8
D3ENNY <denysraimondi06@gmail.com>
2024-03-10 22:46:25 +0100

    initialize homeProduct page

--- src/app/feature/home-page/home-page.component.ts
+++ src/app/feature/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeCarouselComponent } from '../../model/home-carousel/home-carousel.component';
+import { HomeProductComponent } from '../../model/home-product/home-product.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HomeCarouselComponent],
+  imports: [HomeCarouselComponent, HomeProductComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
